f4c99ba80be7f37bb697c6564ec8068e5b1f9544
suresh218 <suresh.alagar@gmail.com>
2016-07-23 20:05:19 +0800

    ColorWheel Class_24July

--- app/src/main/java/com/example/talagarsamysuresh/funfacts/FactBook.java
+++ app/src/main/java/com/example/talagarsamysuresh/funfacts/FactBook.java
@@ -6,14 +6,21 @@ import java.util.Random;
  * Created by T Alagarsamy Suresh on 23/7/2016.
  */
 public class FactBook {
+
+    private String[] mfacts = {"Ants stretch when they wake up in the morning", "Ostriches can run faster than horses",
+            "Olympics are good to watch", "India is a Secular Country", "I born in India", "Born to win"};
+
     public String getFact(){
-        String[] facts = {"Ants stretch when they wake up in the morning", "Ostriches can run faster than horses",
-                "Olympics are good to watch", "India is a Secular Country", "I born in India", "Born to win"};
+
         String fact = "";
         Random randomGenerator = new Random();
-        int randomNumber = randomGenerator.nextInt(facts.length);
-        fact = facts[randomNumber];
+        int randomNumber = randomGenerator.nextInt(mfacts.length);
+        fact = mfacts[randomNumber];
+        //fact=randomNumber + "";
 
         return fact;
     }
 }
+
+
+
--- app/src/main/java/com/example/talagarsamysuresh/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/talagarsamysuresh/funfacts/FunFactsActivity.java
@@ -1,16 +1,22 @@
 package com.example.talagarsamysuresh.funfacts;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
+
     private TextView mFactTextView;
     private Button mShowFactButton;
+    private FactBook mFactBook= new FactBook();
+    private RelativeLayout mRelativeLayout;
+    private ColorWheel mColor = new ColorWheel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +25,18 @@ public class FunFactsActivity extends AppCompatActivity {
 
         mFactTextView=(TextView)findViewById(R.id.FactTextView);
         mShowFactButton=(Button)findViewById(R.id.showFactButton);
+        mRelativeLayout=(RelativeLayout) findViewById(R.id.relativelayout);
+
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String[] facts = {"Ants stretch when they wake up in the morning", "Ostriches can run faster than horses",
-                        "Olympics are good to watch", "India is a Secular Country", "I born in India", "Born to win"};
-                String fact = "";
-               Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(facts.length);
-                fact = facts[randomNumber];
-               //fact=randomNumber + "";
+            public void onClick(View v) {
+              String fact = mFactBook.getFact();
+                int color = mColor.getColor();
                 mFactTextView.setText(fact);
+                mRelativeLayout.setBackgroundColor(color);
+                mShowFactButton.setTextColor(color);
+
+
             }
         };
         mShowFactButton.setOnClickListener(listener);
